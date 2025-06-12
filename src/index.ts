1ca593c67c6764be9f55715204b8b1ad79e6724d
fengshanshan <fengshanshn@icloud.com>
2025-06-12 17:48:57 +0800

    simplify the mcp func

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import {
-  getTextResponseFromGemini,
-  getTextResponseFromOpenAI,
-} from "./utils.js";
 
 import { z } from "zod";
 import "dotenv/config";
@@ -20,51 +16,31 @@ server.tool(
     platform: z
       .string()
       .describe(
-        "The platform of a specific identity, e.g.: Ethereume, farcaster, lens"
+        "The platform of a specific identity, e.g.: Ethereume, farcaster, lens, ens"
       ),
     identity: z.string().describe("User's identity"),
   },
   async ({ platform, identity }) => {
-    let json = null;
     try {
       const request_url =
         process.env.DATA_API_URL + `?identity=${identity}&platform=${platform}`;
       const responseFromAPI = await fetch(request_url, {});
-      json = await responseFromAPI.json();
+      const json = await responseFromAPI.json();
+      const text = JSON.stringify(json.data);
+      return {
+        content: [
+          {
+            type: "text",
+            text: `The related information is: ${text}`,
+          },
+        ],
+      };
     } catch (err) {
       return {
         content: [
           {
             type: "text",
             text: `Error fetching data: ${(err as Error).message}`,
-          },
-        ],
-      };
-    }
-
-    try {
-      const input = `help me get the related address or domain name from the following text: ${json}`;
-      let response = "";
-      if (process.env.MODEL == "gemini") {
-        response = await getTextResponseFromGemini(input);
-      } else {
-        response = await getTextResponseFromOpenAI(input);
-      }
-
-      return {
-        content: [
-          {
-            type: "text",
-            text: `${response}`,
-          },
-        ],
-      };
-    } catch (err) {
-      return {
-        content: [
-          {
-            type: "text",
-            text: `agent process data error: ${(err as Error).message}`,
           },
         ],
       };
